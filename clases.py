d282c0d635931b4e05ab26be64caaa6579d34a5a
joelnvch1 <61578757+joelnvch1@users.noreply.github.com>
2020-12-31 16:45:09 +0100

    Funcionamiento Base

---
+++ clases.py
@@ -0,0 +1,218 @@
+from random import randint
+import threading
+import numpy
+
+DIMENSION_MATRIZ = 4
+
+
+class Cliente:
+    def __init__(self, id):
+        self.id = id
+        self.destino = [randint(0, DIMENSION_MATRIZ - 1), randint(0, DIMENSION_MATRIZ - 1)]
+        self.posicion = []
+        self.pasajero = False
+        self.acaba_salir_de_autobus = False
+
+
+class Taxi:
+    def __init__(self, id):
+        self.id = id
+        self.cliente = None
+        self.posicion = []
+
+    def decidir_mov(self, lista_pos, entorno):
+        if self.cliente is None:
+            raise Exception("Taxi error no deberia estar aqui")
+        direccion_columna = self.cliente.destino[1] - self.posicion[1]
+        direccion_fila = self.cliente.destino[0] - self.posicion[0]
+        pos_resultado = []
+
+        if (abs(direccion_fila) == 1 or abs(direccion_fila) == 0) and (
+                abs(direccion_columna) == 1 or abs(direccion_columna) == 0):
+            # aqui devolvemos la posicion destino siempre que este en el rango pero
+            # puede ser que aunque este en el rango justo nos coincida que
+            # esta ocupado justo en ese momento por otro vehiculo
+            # -arreglar error vahiculoerror
+            if entorno.matriz[self.cliente.destino[0]][self.cliente.destino[1]].vehiculo is None:
+                pos_resultado = self.cliente.destino
+            else:
+                pos_resultado = self.posicion
+
+        else:
+            pos_resultado = lista_pos[0]
+            dist_columna = self.cliente.destino[0] - pos_resultado[0]
+            dist_fila = self.cliente.destino[1] - pos_resultado[1]
+            dist_destino = numpy.linalg.norm([dist_fila, dist_columna])
+
+            for posicion in lista_pos:
+                dist_columna = self.cliente.destino[0] - posicion[0]
+                dist_fila = self.cliente.destino[1] - posicion[1]
+                dist_destino_aux = numpy.linalg.norm([dist_fila, dist_columna])
+                if dist_destino_aux < dist_destino:
+                    pos_resultado = posicion
+                    dist_destino = dist_destino_aux
+
+        if pos_resultado == self.cliente.destino:
+            self.cliente.pasajero = False
+
+        if not pos_resultado:
+            raise Exception('decidir mov error.')
+
+        return pos_resultado
+
+
+class Autobus:
+    def __init__(self, id):
+        self.id = id
+        self.clientes = []
+        self.posicion = []
+
+    def realizar_parada(self, entorno):
+        lista_clientes_fuera = []
+        for cliente in self.clientes:
+            rand = randint(0, 1)
+            if rand == 0:
+                self.clientes.remove(cliente)
+                cliente.pasajero = False
+                cliente.acaba_salir_de_autobus = True
+                entorno.matriz[cliente.posicion[0]][cliente.posicion[1]].clientes.append(cliente)
+                lista_clientes_fuera.append(cliente)
+        return lista_clientes_fuera
+
+    def obtener_clientes(self):
+        res = []
+        for cliente in self.clientes:
+            res.append(cliente.id)
+        return res
+
+
+class Casilla:
+    def __init__(self, id):
+        self.id = id
+        self.estado = threading.Lock()
+        self.vehiculo = None
+        self.clientes = []
+
+
+class Entorno:
+    def __init__(self):
+        self.matriz = numpy.full((DIMENSION_MATRIZ, DIMENSION_MATRIZ), None)
+        for i in range(0, DIMENSION_MATRIZ):
+            for j in range(0, DIMENSION_MATRIZ):
+                self.matriz[i][j] = Casilla([i, j])
+
+    def insertar_elemento(self, elemento, pos_nueva):
+        casilla_dest = self.matriz[pos_nueva[0]][pos_nueva[1]]
+        pos_antigua = elemento.posicion
+
+        if pos_nueva == pos_antigua:
+            pass
+        elif isinstance(elemento, Cliente):
+            # borramos el valor antiguo de la casilla que hemos dejado
+            if pos_antigua and not elemento.acaba_salir_de_autobus:
+                self.matriz[pos_antigua[0]][pos_antigua[1]].clientes.remove(elemento)
+
+            elemento.acaba_salir_de_autobus = False
+            if casilla_dest.vehiculo is None:
+                casilla_dest.clientes.append(elemento)
+                elemento.posicion = pos_nueva
+            # si entra en vehiculo
+            else:
+                # no estoy contemplando que el vehiculo encontrado si es un taxi tengo alguien dentro
+                # -arreglado falta pruebas
+                vehiculo = casilla_dest.vehiculo
+                elemento.posicion = pos_nueva
+                # error#2: hay un caso en el que entra por aqui pero no entra en los ifs
+                # solo se hacen las tres lineas de atras
+                # - arreglado falta pruebas
+                if isinstance(vehiculo, Taxi):
+                    if vehiculo.cliente is None:
+                        elemento.pasajero = True
+                        vehiculo.cliente = elemento
+                        return ["taxi", vehiculo.id]
+                    else:  # si el taxi tiene un cliente
+                        # ponerlo en la casilla
+                        casilla_dest.clientes.append(elemento)
+
+                elif isinstance(vehiculo, Autobus):
+                    vehiculo.clientes.append(elemento)
+                    elemento.pasajero = True
+                    return ["autobus", vehiculo.id]
+
+        # si es un vehiculo
+        else:
+            # tal y como esta diseñado si el valor es un vehiculo es porque es él mismo por lo tanto no hacemos nada
+            if casilla_dest.vehiculo == elemento:
+                return [""]
+            # si el destino no es el mismo y hay un vehiculo dentro da error
+            # esta planteado de forma que a este metodo solo le puedan llegar valores validos
+            elif casilla_dest.vehiculo is not None:
+                raise Exception("Vehiculo error en pos ", pos_nueva, "id del vehiculo ", elemento.id)
+
+            # borramos el valor antiguo de la casilla que hemos dejado
+            if pos_antigua:
+                self.matriz[pos_antigua[0]][pos_antigua[1]].vehiculo = None
+
+            if casilla_dest.vehiculo is None:
+                # movemos vehiculo
+                casilla_dest.vehiculo = elemento
+                elemento.posicion = pos_nueva
+                if isinstance(elemento, Autobus):
+                    for cliente in elemento.clientes:
+                        cliente.posicion = pos_nueva
+                elif isinstance(elemento, Taxi):
+                    if elemento.cliente is not None:
+                        elemento.cliente.posicion = pos_nueva
+                        if elemento.cliente.posicion == elemento.cliente.destino:
+                            # el equivalente a salir del coche y dejarle en la casilla
+                            id_cl = elemento.cliente.id
+                            self.matriz[elemento.cliente.destino[0]][elemento.cliente.destino[1]].clientes.append(
+                                elemento.cliente)
+                            elemento.cliente = None
+                            return ["paradaTaxi", id_cl]
+
+        return [""]
+
+    # priv
+    def casillas_contiguas(self, pos):
+        res = []
+        for i in range(pos[0] - 1, pos[0] + 2):
+            if 0 <= i <= DIMENSION_MATRIZ - 1:
+                for j in range(pos[1] - 1, pos[1] + 2):
+                    if 0 <= j <= DIMENSION_MATRIZ - 1:
+                        res.append(self.matriz[i][j])
+        # la posicion actual la tenemos en el argumento pos, el resto de posiciones en res
+        res.remove(self.matriz[pos[0]][pos[1]])
+        return res
+
+    def lock_alrededor(self, pos):
+        casilla_actual = self.matriz[pos[0]][pos[1]]
+        posiciones_bloqueadas = []
+        # no comienza hasta que no puede bloquear su posicion actual
+        while casilla_actual.estado.locked():
+            pass
+        casilla_actual.estado.acquire()
+        posiciones_bloqueadas.append(casilla_actual.id)
+
+        for casilla in self.casillas_contiguas(pos):
+            if not casilla.estado.locked():
+                casilla.estado.acquire()
+                posiciones_bloqueadas.append(casilla.id)
+        # el primer elemento de esta lista será siempre la casilla actual
+        return posiciones_bloqueadas
+
+    def unlock_casillas(self, lista_pos):
+        for pos in lista_pos:
+            self.matriz[pos[0]][pos[1]].estado.release()
+
+    def casillas_sin_vehiculos(self, lista_pos, boole=True):
+        res = []
+        for pos in lista_pos:
+            if self.matriz[pos[0]][pos[1]].vehiculo is None:
+                res.append(pos)
+        # el primer elemento en la lista es la posicion actual y por lo tanto es una pos valida
+        # este metodo solo lo llaman los vehiculos asi que no ocasionaría ninguna repeticion lo siguiente
+        if boole:
+            # es false en caso de que la lista introducida no provenga de lock_alrededor
+            res.append(lista_pos[0])
+        return res
---
+++ main.py
@@ -0,0 +1,191 @@
+from clases import *
+from random import randint
+from time import sleep
+import numpy as np
+from threading import *
+
+entorno = Entorno()
+n_clientes = 0
+print_lock = Lock()
+threads = []
+
+
+def imprimir(string):
+    print_lock.acquire()
+    for palabra in string:
+        print(palabra, end="")
+    print()
+    print_lock.release()
+
+
+def ciclo_cliente(cliente):
+    global entorno
+    primera_iteracion = True
+
+    while True:
+        if primera_iteracion:
+            pos_disp = [randint(0, DIMENSION_MATRIZ - 1), randint(0, DIMENSION_MATRIZ - 1)]
+            entorno.matriz[pos_disp[0]][pos_disp[1]].estado.acquire()
+            estado = entorno.insertar_elemento(cliente, pos_disp)
+            entorno.matriz[pos_disp[0]][pos_disp[1]].estado.release()
+        else:
+            pos_disp = entorno.lock_alrededor(cliente.posicion)
+            rand_index = randint(0, len(pos_disp) - 1)
+            estado = entorno.insertar_elemento(cliente, pos_disp[rand_index])
+            entorno.unlock_casillas(pos_disp)
+
+        print_lock.acquire()
+        if primera_iteracion:
+            print("Colocacion cliente: ID=", cliente.id, " POS=", cliente.posicion, " PASAJERO?=", cliente.pasajero,
+                  " OBJETO=", cliente)
+        else:
+            print("Cliente Movimiento: ID=", cliente.id, " POS= ", cliente.posicion, " PASAJERO?=", cliente.pasajero)
+        primera_iteracion = False
+
+        if estado[0] == "taxi":
+            print("CLIENTE MONTADO: ID=", cliente.id, " POS=", cliente.posicion, " TAXIID=", estado[1])
+        elif estado[0] == "autobus":
+            print("CLIENTE MONTADO: ID=", cliente.id, " POS=", cliente.posicion, " AUTOBUSID=", estado[1])
+        elif cliente.posicion == cliente.destino:
+            print("EXITO Cliente: ID=", cliente.id, " DEST=", cliente.destino, " {Cliente llega destino}")
+            print_lock.release()
+            # en este caso se llega caminando al destino por lo que la casilla destino siempre contiene al cliente antes de que acabe
+            entorno.matriz[cliente.destino[0]][cliente.destino[1]].clientes.remove(cliente)
+            return 0
+
+        print_lock.release()
+
+        # cuadno cliente montado en vehiculo
+        while cliente.pasajero or entorno.matriz[cliente.posicion[0]][cliente.posicion[1]].estado.locked():
+            pass
+
+        if cliente.posicion == cliente.destino:
+            imprimir(["EXITO Cliente: ID=", cliente.id, " DEST=", cliente.destino])
+            # en este caso la casilla destino puede contener o no el cliente ya que el taxi no deja al cliente en la casilla
+            # solo actualiza su posicion en el objeto, un autobus deja al cliente en la casilla en todo caso
+            entorno.matriz[cliente.destino[0]][cliente.destino[1]].clientes.remove(cliente)
+            return 0
+
+        sleep(4)
+
+
+def ciclo_autobus(autobus):
+    global entorno
+    primera_iteracion = True
+    cont = 0
+
+    while True:
+        if primera_iteracion:
+            primera_iteracion = False
+            pos_posibles = entorno.casillas_sin_vehiculos([[0, 0], [0, DIMENSION_MATRIZ - 1],
+                                                           [DIMENSION_MATRIZ - 1, 0],
+                                                           [DIMENSION_MATRIZ - 1, DIMENSION_MATRIZ - 1]], False)
+            pos_bloqueadas = pos_posibles[randint(0, len(pos_posibles) - 1)]
+            while entorno.matriz[pos_bloqueadas[0]][pos_bloqueadas[1]].vehiculo is not None:
+                pass
+            entorno.matriz[pos_bloqueadas[0]][pos_bloqueadas[1]].estado.acquire()
+            entorno.insertar_elemento(autobus, pos_bloqueadas)
+            entorno.matriz[pos_bloqueadas[0]][pos_bloqueadas[1]].estado.release()
+            imprimir(["Colocacion autobus: ID= ", autobus.id, "  POS= ", autobus.posicion, " CLIENTES= ",
+                      autobus.obtener_clientes()])
+        else:
+            pos_bloqueadas = entorno.lock_alrededor(autobus.posicion)
+            pos_disp = entorno.casillas_sin_vehiculos(pos_bloqueadas)
+            rand_index = randint(0, len(pos_disp) - 1)
+            entorno.insertar_elemento(autobus, pos_disp[rand_index])
+
+            cont = cont + 1
+            # cada 3 movimientos habrá una parada
+            if cont % 3 == 0:
+                list_clientes = autobus.realizar_parada(entorno)
+                entorno.unlock_casillas(pos_bloqueadas)
+                print_lock.acquire()
+                print("Autobus movimiento: ID= ", autobus.id, "  POS= ", autobus.posicion, "  CLIENTES: ",
+                      autobus.obtener_clientes())
+                print("AUTOBUS PARADA: ID=", autobus.id, " POS=", autobus.posicion)
+                for cliente in list_clientes:
+                    print("PASAJERO FUERA: ClienteID= ", cliente.id, "  AutobusDejado= ", autobus.id, "  Posicion= ",
+                          cliente.posicion,
+                          "  pasajero?: ", cliente.pasajero)
+                print_lock.release()
+            else:
+                entorno.unlock_casillas(pos_bloqueadas)
+                imprimir(["Autobus movimiento: ID= ", autobus.id, "  POS= ", autobus.posicion, "  CLIENTES: ",
+                          autobus.obtener_clientes()])
+        sleep(2)
+
+
+def ciclo_taxi(taxi):
+    global entorno
+    primera_iteracion = True
+
+    while True:
+        # Ponerlo en la matriz
+        if primera_iteracion:
+            primera_iteracion = False
+            pos = [randint(0, DIMENSION_MATRIZ - 1), randint(0, DIMENSION_MATRIZ - 1)]
+            while entorno.matriz[pos[0]][pos[1]].vehiculo is not None:
+                pass
+            entorno.matriz[pos[0]][pos[1]].estado.acquire()
+            entorno.insertar_elemento(taxi, pos)
+            entorno.matriz[pos[0]][pos[1]].estado.release()
+            imprimir(["Colocacion Taxi: ID= ", taxi.id, "  POS= ", taxi.posicion, "  CLIENTEisNone?= ", taxi.cliente is None])
+        else:
+            pos_bloqueadas = entorno.lock_alrededor(taxi.posicion)
+            pos_disp = entorno.casillas_sin_vehiculos(pos_bloqueadas)
+
+            if taxi.cliente is None:
+                if len(pos_disp) == 1:
+                    rand_index = 0
+                if len(pos_disp) <= 0:
+                    raise Exception("problema con casillas_sin_vehiculos o lock_alrededor")
+                else:
+                    rand_index = randint(0, len(pos_disp) - 1)
+                code = entorno.insertar_elemento(taxi, pos_disp[rand_index])
+            else:
+                pos = taxi.decidir_mov(pos_disp, entorno)
+                code = entorno.insertar_elemento(taxi, pos)
+
+            entorno.unlock_casillas(pos_bloqueadas)
+
+            print_lock.acquire()
+            if code[0] == "paradaTaxi":
+                print("Taxi movimiento: ID= ", taxi.id, " POS=", taxi.posicion, " CLIENTEisNone= [", taxi.cliente is None, "}")
+                print("TAXI PARADA: ID= ", taxi.id, " POS=", taxi.posicion, " {Cliente ", code[1], " se baja}")
+            else:
+                print("Taxi movimiento: ID= ", taxi.id, " POS=", taxi.posicion, " CLIENTEisNone= [", taxi.cliente is None, "}")
+            print_lock.release()
+        sleep(1)
+
+
+if __name__ == "__main__":
+    # El número de clientes disponibles, de autobuses y de taxistas debe ser configurable al comenzar el juego.
+    n_clientes = int(input("Introducir número de clientes: \n"))
+    n_autobuses = int(input("Introducir número de autobuses: \n"))
+    n_taxistas = int(input("Introducir número de taxistas: \n"))
+    print()
+
+    elementos_set = set()
+    for i in range(1, n_clientes + 1):
+        elementos_set.add(Cliente(i))
+    for i in range(1, n_autobuses + 1):
+        elementos_set.add(Autobus(i))
+    for i in range(1, n_taxistas + 1):
+        elementos_set.add(Taxi(i))
+
+    for elemento in elementos_set:
+        targ = None
+
+        if isinstance(elemento, Cliente):
+            targ = ciclo_cliente
+        elif isinstance(elemento, Autobus):
+            targ = ciclo_autobus
+        elif isinstance(elemento, Taxi):
+            targ = ciclo_taxi
+
+        t = Thread(target=targ, args=(elemento,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
